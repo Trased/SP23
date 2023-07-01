77cde63263641a29827094289acd8a55d5a3016a
Trased <117777557+Trased@users.noreply.github.com>
2023-07-01 20:12:18 +0300

    Create Concatenator_design.v

---
+++ Day2/Concatenator_design.v
@@ -0,0 +1,8 @@
+module task2_concatenator(InA, InB, InC, InD, InE, out);
+  input [7:0] InA, InB, InC;
+  input [3:0] InD, InE;
+  output [31:0] out;
+  
+  assign out = {InE, InD, InC, InB, InA};
+  
+endmodule
