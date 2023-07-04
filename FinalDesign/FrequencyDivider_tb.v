dab6094da9fba4cabbd4adaf4231fc63220e0f8a
Trased <117777557+Trased@users.noreply.github.com>
2023-07-04 22:53:47 +0300

    Create FrequencyDivider_tb.v

---
+++ FinalDesign/FrequencyDivider_tb.v
@@ -0,0 +1,39 @@
+module tb;
+reg [31:0] Din;
+reg ConfigDiv, Reset, Clk, Enable;
+wire ClkOut;
+
+FrequencyDivider DUT(
+                    .Din(Din),
+                    .ConfigDiv(ConfigDiv),
+                    .Reset(Reset),
+                    .Clk(Clk),
+                    .Enable(Enable),
+                    .ClkOut(ClkOut));
+ wire [31:0] T, counter;
+ assign T = DUT.T;
+ assign counter = DUT.counter;
+ 
+ initial
+ begin
+    #0 Clk <= 1'b0;
+    forever #5 Clk <= ~Clk;
+ end                 
+
+initial
+begin
+    #0
+        Din <= 32'h00000002;
+        ConfigDiv <= 1'b0;
+        Reset <= 1'b1;
+        Enable <= 1'b0;
+    #10
+        ConfigDiv <= 1'b1;
+        Reset <= 1'b0;
+    #20
+        ConfigDiv <= 1'b0;
+        Enable <= 1'b1;
+    #300
+        $finish;
+end
+endmodule
