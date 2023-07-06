3fa84bd5723c5c0eb50b8651c054075eb83ad2b6
Trased <117777557+Trased@users.noreply.github.com>
2023-07-06 08:46:35 +0300

    Update TopModule.v

--- FinalDesign/TopModule.v
+++ FinalDesign/TopModule.v
@@ -5,47 +5,50 @@ input[3:0] Sel,
 input[31:0] DIn,
 output CalcActive, CalcMode, Busy, DOutValid, DataOut, ClkTx);
 
-reg ResetTmp, RWTmp;
-wire [31:0] ConcatOutTmp, MemOutTmp, TxDinTmp;
-wire [7:0] MuxInATmp, MuxInBTmp, AluOutTmp;
+wire [31:0] ConcatOutTmp, MemDout, TxDinTmp;
+wire [7:0] MuxInATmp, MuxInBTmp, AluOutTmp; 
 wire [3:0] MuxSelTmp, AluFlagTmp;
-wire ActiveTmp, CtrlModeTmp, CtrlTransferDataTmp, CtrlRWMemTmp, CTRLAccessMemTmp, SampleData, TxData;
+wire SampleData, StartTx, CtrlTransferDataTmp, ClkTmp, Active, CtrlRWMemTmp, CtrlAccessMemTmp, CtrlModeTmp;
+reg ResetTmp, RWTmp;
+
 
 always@(*)
 begin
-    if(!ActiveTmp || Reset)
+    if(!Active || Reset)
         ResetTmp <= 1'b1;
     else
         ResetTmp <= 1'b0;
-    if(ActiveTmp && RWMem)
+    if(Active && RWMem)
         RWTmp <= 1'b1;
     else
         RWTmp <= 1'b0;
 end
 
-
-assign CalcActive = ActiveTmp;
+assign CalcActive = Active;
 assign CalcMode = CtrlModeTmp;
+assign ClkTx = ClkTmp;
 
-Mux8Bit #(8) MuxA_DUT(A, ResetTmp, MuxInATmp);
+Mux8Bit #(8) MuxA_DUT(InA, ResetTmp, MuxInATmp);
 
-Mux8Bit #(8) MuxB_DUT(B, ResetTmp, MuxInBTmp);
+Mux8Bit #(8) MuxB_DUT(InB, ResetTmp, MuxInBTmp);
 
 Mux8Bit #(4) MuxSel_DUT(Sel, ResetTmp, MuxSelTmp);
 
 ALU ALU_DUT(MuxInATmp, MuxInBTmp, MuxSelTmp, AluOutTmp, AluFlagTmp);
 
-DecInputKey InputKey_DUT(InputKey,ValidCmd, Reset, Clk, ActiveTmp, CtrlModeTmp);
+DecInputKey InputKey_DUT(InputKey, ValidCmd, ResetTmp, Clk, Active, CtrlModeTmp);
 
-RW_flow Controller_DUT(Active, Mode, ValidCmd, RWTmp, Reset, Clk, CtrlTransferDataTmp,CTRLAccessMemTmp, CtrlRWMemTmp, SampleData, TxData, Busy);
+RW_flow Controller_DUT(Active, CtrlMode, ValidCmd, RWTmp, ResetTmp, Clk, CtrlTransferDataTmp, CtrlAccessMemTmp, CtrlRWMemTmp, SampleData, TxData, Busy);
 
 Concatenator Concatenator_DUT(MuxInATmp, MuxInBTmp, AluOutTmp, MuxSelTmp, AluFlagTmp, ConcatOutTmp);
 
-Memory #(8, 32) Memory_DUT(ConcatOutTmp, Addr, CtrlRWMemTmp, CtrlAccessMemTmp, ResetTmp, Clk, MemOutTmp);
+Memory #(8, 32) Memory_DUT(DIn, Addr, CtrlRWMemTmp, CtrlAccessMemTmp, ResetTmp, Clk, MemDout);
 
-Mux32Bit #(32) Mux32Bit_DUT(ConcatOutTmp, MemOutTmp, CtrlModeTmp, TxDinTmp);
+Mux32Bit #(32) Mux32Bit_DUT(ConcatOutTmp, MemDout,CtrlModeTmp, TxDinTmp);
 
-SerialTransciever #(32) SerialTransciever_DUT(TxDinTmp, SampleData, TxData, ResetTmp, Clk, ClkTx, CtrlTransferDataTmp, DOutValid, DataOut);
+SerialTransciever #(32) SerialTransciever_DUT(TxDinTmp, SampleData, StartTx, ResetTmp, Clk, ClkTmp, CtrlTransferDataTmp, DoutValid, DataOut);
 
-FrequencyDivider FrequencyDivider_DUT(DIn, ConfigDiv, ResetTmp, Clk, ActiveTmp, ClkTx);
+FrequencyDivider FrequencyDivider_DUT(DIn, ConfigDiv, ResetTmp, Clk, Active, ClkTmp);
+
+
 endmodule
