4c6e76f65e969023c7b2bb65daa89b3ef5626c7c
Trased <117777557+Trased@users.noreply.github.com>
2023-07-21 15:00:59 +0300

    Update Test.sv

--- FinalDesign/SystemVerilog/Test.sv
+++ FinalDesign/SystemVerilog/Test.sv
@@ -1,27 +1,33 @@
-class test;
-    env e0;
-    mailbox drv_mbx;
-    
-    function new();
-        drv_mbx = new();
-        e0 = new();
-    endfunction
-    
-    virtual task run();
-        e0.d0.drv_mbx = drv_mbx;
-        fork
-            e0.run();
-        join_none
-        
-        apply_stim();
-    endtask
-    
-    virtual task apply_stim();
-        Top_item item;
-        $display("T=%0t [Test] Starting stimulus ...", $time);
-        item = new;
-         // TO FINISH TEST AND TESTBENCH
-         // ALSO TO REVIEW THE SCOREBOARD LOGIC!!!!
-    endtask
+////
+////
+//// NU ESTE FOLOSIT !!!!!
+////
+////
 
-endclass
+// class test;
+//     env e0;
+//     mailbox drv_mbx;
+    
+//     function new();
+//         drv_mbx = new();
+//         e0 = new();
+//     endfunction
+    
+//     virtual task run();
+//         e0.d0.drv_mbx = drv_mbx;
+//         fork
+//             e0.run();
+//         join_none
+        
+//         apply_stim();
+//     endtask
+    
+//     virtual task apply_stim();
+//         Top_item item;
+//         $display("T=%0t [Test] Starting stimulus ...", $time);
+//         item = new;
+//          // TO FINISH TEST AND TESTBENCH
+//          // ALSO TO REVIEW THE SCOREBOARD LOGIC!!!!
+//     endtask
+
+// endclass
